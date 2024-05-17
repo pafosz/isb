22a13284bc7d103738ccf7871038807fe6231889
pafosz <124775152+pafosz@users.noreply.github.com>
2024-05-18 03:17:09 +0400

    The card number and dependency graph are received

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,6 +1,13 @@
 import multiprocessing as mp
 
-from modules import work_with_files
+from modules import work_with_files, number_card
 
-cores = mp.cpu_count()
-print("Ядер на железе:", cores)
+
+if __name__ == "__main__":
+    hash = work_with_files.read_json('card_information.json')['hash']
+    last_digits = work_with_files.read_json('card_information.json')['last_digits']
+    bins = work_with_files.read_json('card_information.json')['bins']
+    # mp.freeze_support()
+    # number_card.time_to_search_for_collision(hash, last_digits, bins)
+    print(work_with_files.read_json('card_number.json'))
+    print(number_card.algorithm_Luna(work_with_files.read_json('card_number.json')))
--- lab_4/modules/number_card.py
+++ lab_4/modules/number_card.py
@@ -1,27 +1,55 @@
 import multiprocessing as mp
 from matplotlib import pyplot as plt
 import hashlib as hl
+import time
+import numpy as np
 
+from tqdm import tqdm
 from modules import work_with_files
 
-def generate_card_number(bins: list, guessed_digits: int, last_digit: str)->str:
-    return f'{bin}{str(guessed_digits).zfill(6)}{last_digit}'
+
+def checking_card_by_hash(bins: list, guessed_digit: int, last_digits: int, hash: str)->str:
+    
+    for bin in bins:
+        guessed_card_number = f'{str(bin)}{str(guessed_digit).zfill(6)}{str(last_digits)}'
+        if hl.sha224(guessed_card_number.encode()).hexdigest() == hash:
+            return guessed_card_number
 
 
-def comparing_card_with_hash(card_number: str, hash: str)->bool:
-    return hl.sha224(card_number.encode()).hexdigest() == hash
-
-
-def select_card_number(hash: str, last_digits: int, bins: list)->str:    
-    with mp.Pool(processes=mp.cpu_count()) as p:
-        for res in p.map(select_card_number, [(i, last_digits, hash) for i in range(0, 999999)]):
-            if res:                
-                work_with_files.writing_to_json(res, "output_data/card_number.json")
-                print(f"Мы нашли {res} при количестве: {mp.cpu_count()} процессов и закрыли пул")
+def select_card_number(hash: str, last_digits: int, bins: list, count_cpu: int)->str:    
+    
+    with mp.Pool(count_cpu) as p:              
+        for res in p.starmap(checking_card_by_hash, [(bins, i, last_digits, hash) for i in list(range(0, 999999))]):                
+            if res:                   
+                work_with_files.writing_to_json(res, "card_number.json")
+                print(f"Мы нашли {res} при количестве: {count_cpu} процессов и закрыли пул")
                 p.terminate()
                 return res
             
 
 def algorithm_Luna(card_number: str)->bool:
-    pass
+    reverse_card_number = [int(digit) for digit in card_number[::-1]]
+    for i in range(len(reverse_card_number)):
+        if i % 2 != 0:
+            reverse_card_number[i] *= 2
+            if reverse_card_number[i] > 10:
+                reverse_card_number[i] = (reverse_card_number[i] % 10) + (reverse_card_number[i] // 10)
+    return sum(reverse_card_number) % 10 == 0
 
+
+def time_to_search_for_collision(hash: str, last_digits: int, bins: list)->None:
+    time_list = []
+    for count_cpu in tqdm(range(1, int(1.5 * mp.cpu_count())), desc="Поиск коллизии"):
+        start = time.time()
+        if select_card_number(hash, last_digits, bins, count_cpu):
+            time_list.append(time.time() - start)
+
+    x = list(range(1, int(1.5 * mp.cpu_count())))
+    y = time_list
+
+    plt.figure(figsize=(30,5))
+    plt.xlabel('Количество процессов')
+    plt.ylabel('Время выполнения, c')
+    plt.title('Зависимость количества процессов от времени')    
+    plt.plot(x, y, color='m', linestyle='--', marker='x', linewidth=1, markersize=4)
+    plt.show()
